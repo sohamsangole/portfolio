b816a5235c7d4b920adef261536f17c42f5dee91
Soham Sangole <sasangole2019@gmail.com>
2026-08-30 23:34:45 +0530

    Add GitHub Pages deployment workflow and configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://sohamsangole.github.io',
+  base: '/portfolio',
   integrations: [tailwind({
     applyBaseStyles: false,
   })],
