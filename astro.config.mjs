eebcdd1944966f77bbe65b7fff854b5a74bb5d29
Soham Sangole <sasangole2019@gmail.com>
2026-09-13 23:27:52 +0530

    Make theme switch smooth with text and icon, and add profile photo preview

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   site: 'https://sohamsangole.github.io',
   base: process.env.NODE_ENV === 'production' ? '/portfolio' : '/',
+  server: {
+    host: true,
+  },
   integrations: [tailwind({
     applyBaseStyles: false,
   })],
